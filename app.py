8977920e8023a2c08e034c81b2fa22db553bedc8
pdangelo4 <paulsdangelo@gmail.com>
2019-09-26 22:15:42 -0700

    flask hw

--- app.py
+++ app.py
@@ -4,23 +4,22 @@ app = Flask(__name__)
 
 
 
-
+# home page
 @app.route("/")
 def home_route():
     return "Hello world"
 
-
-
+# greeting route
 @app.route('/greeting')
 def greeting(name = 'Paul'):
     return render_template('/index.html', name = name)
 
 
 ingredients = ['flour', 'apples', 'eggs']
-
+#pie ingredients route
 @app.route('/pie')
 def pie():
-    return jsonify({ '"pie ingredient"': ingredients[0]})
+    return jsonify({ 'pie ingredient': ingredients[0]})
 
 
 # listen on port 5000
