78227596b42ff008c778d2ab69ffda0d4d50491a
stanstaton <thatkidenzo@gmail.com>
2019-09-26 20:12:06 -0700

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #import stuff
-from flask import Flask
+from flask import Flask, render_template
 
 #declare app
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #home route
 @app.route('/')
 def home():
-    return 'I guess this is it'
+    return render_template('index.html', name='Akilah')
 
 if __name__ == '__main__':
     app.run()
