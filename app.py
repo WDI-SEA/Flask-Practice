793d8649cb783d2a33edd108570c1e29cdeeeabb
Kirk Hilse <khilsedev@gmail.com>
2019-09-26 20:46:50 -0700

    Added pie API route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import random
 
 # Declare app variable
 app = Flask(__name__)
@@ -11,6 +12,13 @@ def home_route():
 @app.route('/greeting')
 def greeting(user = 'Kirk'):
     return render_template('/index.html', user = user)
+
+@app.route('/pie')
+def pie():
+    return jsonify({ '"pie ingredient"': random.choice(pie_ingredients) })
+
+pie_ingredients = ['flour', 'butter', 'eggs']
+
 
 # Listener
 if __name__ == "__main__":
