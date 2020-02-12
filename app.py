63f5de15b6c2d1c792dcc53d3c159ab8670f4b07
yboodhan <yboodhan@gmail.com>
2020-02-11 16:59:39 -0800

    Completed problem 3 and 4

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import any needed packages including but not limited to flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import random
 # Declare app instance
 app = Flask(__name__)
 
@@ -11,6 +12,12 @@ def home():
 @app.route('/greeting')
 def greeting():
     return render_template('index.html', name='Akilah')
+
+@app.route('/pie', methods=["GET"])
+def pie():
+    ingredients = ['flour', 'sugar', 'filling']
+    return jsonify({'pie ingredient': ingredients[random.randint(0,len(ingredients) - 1)]})
+
 
 # Get the server up and running
 if __name__ == '__main__':
