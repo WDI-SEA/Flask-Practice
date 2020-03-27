3dec7df03cffcac3712e4e827d4cd36f5938963d
CCLancaster <connieclancaster@gmail.com>
2020-03-27 08:52:57 -0700

    made it most the way through bonus, including the get route and including form, still working on for loop in html and getting a post route working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
 
 app=Flask(__name__)
 
@@ -11,8 +11,16 @@ def greetings():
     name = "Akilah"
     return render_template('index.html', name=name)
 
+ingredients = ["custard", "chocolate", "butter"]
+
 @app.route('/pie')
 def pie_pie():
-    ingredients = ["custard", "chocolate", "butter"]
     return jsonify({'pie ingredient' : ingredients[0]})
 
+@app.route('/recipe', methods=["POST", "GET"])
+def recipe_data():
+    if request.method == "GET":
+        return render_template('pie.html', name="Custard Pie")
+    else:
+        form_data = ingredients.append(ingredients)
+        return redirect('/recipe')
