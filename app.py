131c08397f8bf202b5a72e9a64486d29d0e30442
snipspin <photonenimpuls@gmail.com>
2020-03-26 18:13:41 -0700

    problem 2

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
     return "Hello World"
+
+@app.route('/greeting')
+def greeting():
+    name = "Akilah"
+    return render_template('index.html', name=f'{name}')
