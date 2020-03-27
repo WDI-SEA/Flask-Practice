be55b6a051b242ae5728c2e2b815287bec16d82a
CCLancaster <connieclancaster@gmail.com>
2020-03-27 08:30:17 -0700

    finished problem 3 & 4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app=Flask(__name__)
 
@@ -10,3 +10,9 @@ def hello_world_of_pie():
 def greetings():
     name = "Akilah"
     return render_template('index.html', name=name)
+
+@app.route('/pie')
+def pie_pie():
+    ingredients = ["custard", "chocolate", "butter"]
+    return jsonify({'pie ingredient' : ingredients[0]})
+
