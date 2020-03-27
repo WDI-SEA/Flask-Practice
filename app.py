7ec0d4efa2d65c56466838f671ace7e35809ad55
segwuonwu <segwuonwu@gmail.com>
2020-03-27 10:25:07 -0700

    Added a form for my pie

--- app.py
+++ app.py
@@ -11,13 +11,30 @@ def home():
 def greeting():
     return render_template('index.html', name="Constance")
 
+
 ingredients = ['Milk', 'Butter', 'Eggs', 'Flour', 'Ground beef', 'Potatoes']
 
-@app.route('/pie')
+@app.route('/pie', methods=['GET', 'POST'])
 def pie():
     global ingredients
-    random.shuffle(ingredients)
-    for i in ingredients:
-        return jsonify({'pie ingredient': i})
+    if request.method == 'POST':
+        # add submitted user ingredient to existing list
+        ingredient = request.form['ingredient']
+        ingredients.append(ingredient)
+        print(ingredients)
+
+        random.shuffle(ingredients)
+        for i in ingredients:
+            return jsonify({'pie ingredient': i})
+
+        # call random index
+        # index = random.randint(0, (len(ingredients)-1))
+        # return jsonify({'pie ingredient': ingredients[index]})
+    else:
+        return render_template('pie.html')
+
+    # random.shuffle(ingredients)
+    # for i in ingredients:
+    #     return jsonify({'pie ingredient': i})
     
         
