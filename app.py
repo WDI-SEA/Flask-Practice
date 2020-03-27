7f8cca89c47eb69ecb924cee876248f4d9ae3f98
dlew253 <dlew253@yahoo.com>
2020-03-27 10:21:56 -0700

    minor fixes

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request, jsonify
 import random
 
 app=Flask(__name__)
-
+#1
 @app.route('/')
 def hello_world():
     return "Hello, world"
-
+#2
 @app.route('/greeting')
 def greeting():
     return render_template('index.html', name='Dylan')
-
+#3
 ingredients = ['peaches', 'blueberries', 'crust']
 
 @app.route('/pie', methods=['GET', 'POST'])
@@ -21,7 +21,7 @@ def pie():
         ingredient = request.form['ingredient']
         ingredients.append(ingredient)
 #set index
-        index = random.radiant(0, (len(ingredients) -1))
+        index = random.randint(0, (len(ingredients) -1))
         return jsonify({'pie ingredient': ingredients[index]})
     else:
         return render_template('piePage.html')
