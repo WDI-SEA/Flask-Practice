1e14d2da13fac7d32ccca77834dbfc575494c393
deanjheikkila <deanjheikkila@gmail.com>
2020-03-27 10:19:05 -0700

    in class edits with Anna

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import random
 
 app=Flask(__name__)
 
@@ -8,15 +9,29 @@ def hello_world():
 
 @app.route('/greeting')
 def greeting():
-    name = "Anna"
-    greeting = "Hi"
+    name = "Leif Eriksson"
+    greeting = "Ahoy"
     return render_template('index.html', name=name, greeting=greeting)
 
-@app.route('/pie')
+ingredients = [
+    'apples',
+    'sugar',
+    'flour'
+]
+
+@app.route('/pie', methods=['GET', 'POST'])
 def pie():
-    ingredients = [
-        'apples',
-        'sugar',
-        'flour'
-    ]
-    return jsonify({'pie ingredient': ingredients[0]})
+    if request.method == 'POST':
+        global ingredients
+
+        #add submitted user ingredient to existing list
+        ingredient = request.form["ingredient"]
+        ingredients.append(ingredient)
+        print(ingredients)
+
+        # call random index
+        index = random.randint(0, (len(ingredients)-1))
+
+        return jsonify({'pie ingredients': ingredients, 'random ingredient': ingredients[index]})
+    else:
+        return render_template('pieform.html')
