9723df27255f468502f4b90e156760e8245cc583
dlew253 <dlew253@yahoo.com>
2020-03-26 22:47:28 -0700

    works but no CSS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import random
 
 app=Flask(__name__)
 
@@ -6,7 +7,22 @@ app=Flask(__name__)
 def hello_world():
     return "Hello, world"
 
-@app.route('/owner')
-def tell_owner():
-    owner = "Anna"
-    return render_template('index.html', owner=owner)
+@app.route('/greeting')
+def greeting():
+    return render_template('index.html', name='Dylan')
+
+ingredients = ['peaches', 'blueberries', 'crust']
+
+@app.route('/pie', methods=['GET', 'POST'])
+def pie():
+    global ingredients 
+    if request.method == 'POST':
+# needs user inouts and add to ingredients 
+        ingredient = request.form['ingredient']
+        ingredients.append(ingredient)
+#set index
+        index = random.radiant(0, (len(ingredients) -1))
+        return jsonify({'pie ingredient': ingredients[index]})
+    else:
+        return render_template('piePage.html')
+    
