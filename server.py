b70b6ca297c784da235666a15f9dadb80f50c84b
ObtuseSpruce <williamllapitan@gmail.com>
2020-06-02 08:45:03 -0700

    will - hw

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import jsonify
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+@app.route('/pie', methods=['GET'])
+def pie():
+    ingredients =['apples', 'sugar', 'flour', 'allspice', 'butter']
+    return jsonify({'pie ingredient': ingredients[0]})
+
+if __name__ == "__main__":
+    app.run()
