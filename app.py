ecb9be2c27fc60fab4375853962c9fdf37095e11
gjones81 <jones.guy81@gmail.com>
2020-06-02 09:43:51 -0700

    Updating

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import random
 
-from flask import Flask
-from flask import render_template
-from flask import jsonify
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+ingredients = ['apples', 'butter', 'sugar', 'flour',]
 
 @app.route('/')
 def hello_world():
@@ -16,9 +16,18 @@ def greeting():
 
 @app.route('/pie')
 def pie():
-    ingredients = ['apples', 'butter', 'sugar', 'flour',]
     random_number = random.randint(0, 3)
     return jsonify({'pie ingredient': ingredients[random_number]})    
+
+@app.route('/recipe', methods = ['GET', 'POST'])
+def recipe():
+    if request.method == 'POST':
+        new_ingredient = request.form['new_ingredient']
+        ingredients.append(new_ingredient)
+        random_number = random.randint(0, len(ingredients))
+        return render_template('recipe.html', ingredients = ingredients)  
+    else:
+        return render_template('recipe.html', ingredients = ingredients)  
 
 if __name__ == '__main__':
     app.run()
