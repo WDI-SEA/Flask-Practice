52034cacd2775733525a0882c6b645cb778f454f
d4vves <d4vves@gmail.com>
2020-08-03 16:49:17 -0500

    problem two

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/greeting')
+def greeting():
+    return render_template('greeting.html', name='Akilah')
