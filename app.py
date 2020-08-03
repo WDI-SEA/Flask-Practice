f0466a0ef45e81b581031e0396cfaca56892c4e3
Nick Foxen <foxen.nicholas@gmail.com>
2020-08-03 15:37:25 -0700

    Step 1 - 3 completed

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return "Hello, World!"
+
+@app.route('/greeting/<name>')
+def greeting(name):
+  return render_template('index.html', name='Akilah')
+
