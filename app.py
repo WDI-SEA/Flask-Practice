16195753c3f2df2608e2354548522716dc22c14d
Weston Bailey <void@MBP.local>
2020-08-03 17:36:06 -0700

    mvp and bonus

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ ingredients = [
 
 @app.route('/pie')
 def pie():
+  # get random ingredient
   ingredients_length = len(ingredients)
   random_index = random.randrange(0, ingredients_length, 1)
   return jsonify({'pie ingredient': ingredients[random_index]})
@@ -35,9 +36,20 @@ pies = {
   'apple': ingredients
 }
 
-print(pies)
-
 @app.route('/recipe', methods=['GET', 'POST'])
 def recipe():
-  pieJson = jsonify(pies)
-  return render_template('recipe.html')
+  # POST req from form
+  if request.method == 'POST':
+    # get pies object
+    global pies
+    # form input
+    ingredient = request.form['ingredient']
+    pie = request.form['pie']
+    # check if the pie exists already or make it
+    if pie in pies:
+      pies[pie].append(ingredient)
+    else:
+      pies[pie] = [ingredient] 
+    print(pies)
+  # return pies for all requests
+  return render_template('recipe.html', pies=pies)
