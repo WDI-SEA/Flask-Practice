3fc13f0b0e4d1576b47b2553b87eb0423319f5b3
d4vves <d4vves@gmail.com>
2020-08-03 20:47:33 -0500

    bonus

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, redirect, request
+import random
+
 app = Flask(__name__)
+
+ingredients = ['apple', 'cherry', 'meat']
+recipes = {
+    'apple': ['apples', 'flour'],
+    'cherry': ['cherries', 'flour'],
+    'peach': ['peaches', 'flour']
+}
 
 @app.route('/')
 def hello_world():
@@ -8,3 +17,20 @@ def hello_world():
 @app.route('/greeting')
 def greeting():
     return render_template('greeting.html', name='Akilah')
+
+@app.route('/pie')
+def pie():
+    ingredient = random.choice(ingredients)
+    return jsonify({'pie ingredient': ingredient})
+
+@app.route('/recipe/<pie>', methods = ['GET', 'POST'])
+def recipe(pie):
+    if (request.method == 'GET'):
+        for recipe in recipes:
+            return render_template('recipe.html', name = pie, ingredients = recipes[pie])
+    if (request.method == 'POST'):
+        new_ingredient = request.form['ingredient']
+        redirect_key = request.form['redirect']
+        for recipe in recipes:
+            recipes[pie].append(new_ingredient)
+            return redirect(f'/recipe/{pie}')
