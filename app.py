6a9db243eccfd8129dedbdb456aaeaf1d5e9287b
Nick Hvattum <nhvattum@gmail.com>
2020-08-03 22:48:34 -0700

    MVP

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,10 @@ def hello_world():
 @app.route('/greeting/<name>')
 def greeting(name):
     return render_template('index.html', name=name)
+
+ingredients = ["sugar", "flour", "butter", "fruit"]
+
+@app.route('/pie')
+def pie():
+    global ingredients
+    return jsonify({'pie ingredient': ingredients[1]})
