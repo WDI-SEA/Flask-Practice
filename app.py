0bdcadc0aae8a22f2adac99f5e56a39fecf43408
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-08-03 20:14:23 -0500

    Finished pt 2, working on pt 3

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/greetings/<name>')
+def greeting(name):
+    return render_template('index.html', name=name)
